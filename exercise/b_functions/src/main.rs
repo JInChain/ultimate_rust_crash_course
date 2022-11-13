6cd41e3ebf18387de9af12c076666cf941395227
JInChain <jakubinchain@gmail.com>
2022-11-13 17:52:42 +0100

    test2

--- exercise/b_functions/src/main.rs
+++ exercise/b_functions/src/main.rs
@@ -1,5 +1,5 @@
-// Silence some warnings so they don't distract from the exercise.TEST
-#![allow(unused_variables)]
+// Silence some warnings so they don't distract from the exercise.
+#![allow(unused_variables)] 
 
 fn main() {
     let width = 4;
